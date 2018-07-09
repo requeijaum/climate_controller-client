4f56146df97a8b0df8875fda2e59bfa6e3f79571
Rafael Requião <rafaelfrequiao@gmail.comm>
2018-07-09 10:28:36 -0300

    Tudo funcionando lindamente. Corrigi os caminhos apontados no script install_apk_debug.sh e dei checkout no config.xml aqui no macOS pra não causar conflito.

--- install_apk_debug.sh
+++ install_apk_debug.sh
@@ -1,1 +1,1 @@
-/Users/rafaelfrequiao/Library/Android/sdk/platform-tools/adb install ~/ionic/projects/MyIonicProject/platforms/android/app/build/outputs/apk/debug/app-debug.apk
+~/Library/Android/sdk/platform-tools/adb install ./platforms/android/app/build/outputs/apk/debug/app-debug.apk
